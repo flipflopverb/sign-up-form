7e53ea38231894a8ecfebff5d8826e0a5bdd3942
flipflopverb <ramah808@gmail.com>
2023-10-19 18:48:12 -0700

    added some validation

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+function checkPasswords() {
+  const passA = document.getElementById('password').value;
+  const passB = document.getElementById('confirmPassword').value;
+  /*  alert('you pushed submit'); */
+  if (passA == passB) {
+    alert('match ' + passA + ' ' + passB);
+    passA.className('valid');
+    passB.className('valid');
+    return true;
+  } else {
+    alert('do not match ' + passA + ' ' + passB);
+    passA.className('invalid');
+    passB.className('invalid');
+    return false;
+  }
+}
 
+/* const myform = document.getElementById('form');
+myform.addEventListener('submit', checkPasswords); */
